8fb13b0e228f6cb6b46c6bc1f2eb1981a867e81a
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-10-02 10:06:27 -0700

    add filters for tahoe>0.7

---
+++ filters/__init__.py
@@ -0,0 +1,3 @@
+from .filt_cowrie import filt_cowrie
+from .filt_misp import filt_misp
+from .filt_openphish import filt_openphish
---
+++ filters_legacy/README.md
@@ -0,0 +1,3 @@
+# Info
+Filters for use with tahoe<0.7 and CYBEX-P/cybexp-cs   
+
