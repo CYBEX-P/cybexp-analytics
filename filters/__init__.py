b7163428448ff6df57b2713c48861b00f770c957
Ignacio Astaburuaga <ignaciochg@hotmail.com>
2020-10-22 12:56:35 -0700

    Update __init__.py

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,3 +1,3 @@
 from .filt_cowrie import filt_cowrie
-from .filt_phishtank import filt_phishtank
-from .filt_openphish import filt_openphish
+#from .filt_phishtank import filt_phishtank
+#from .filt_openphish import filt_openphish
